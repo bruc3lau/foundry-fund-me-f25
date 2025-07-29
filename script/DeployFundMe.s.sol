28f0c559d2c38954dc7b7224618126f0da3754cd
Bruce Lau <liulqx@gmail.com>
2025-07-29 23:25:33 +0800

    feat: fundme test

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {console} from "forge-std/console.sol";
-import {FundMe} from "../src/FundMe.sol";
+import {FundMe} from "../../src/FundMe.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
